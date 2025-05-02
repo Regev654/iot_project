9b761bc684d0bd04367fb6cab9774122e77f8aa0
regev654 <regev654@gmail.com>
2025-05-02 04:13:56 +0300

    adding notes to the wire connection

--- ESP32/clion_proj/parameters.h
+++ ESP32/clion_proj/parameters.h
@@ -2,11 +2,11 @@
 #define LED_PIN		    10
 #define PIXELS_COUNT    3
 #define PRINTER_UART    1
-#define PRINTER_RX_PIN  16 //the rx pin of the printer
-#define PRINTER_TX_PIN  17 //the tx pin of the printer
+#define PRINTER_RX_PIN  16 //the rx pin of the printer (white wire)
+#define PRINTER_TX_PIN  17 //the tx pin of the printer (blue wire)
 #define BARCODE_UART    2
-#define BARCODE_RX_PIN  5 //the rx pin of the barcode scanner
-#define BARCODE_TX_PIN  4 //the tx pin of the barcode scanner
+#define BARCODE_RX_PIN  5 //the rx pin of the barcode scanner (blue wire)
+#define BARCODE_TX_PIN  4 //the tx pin of the barcode scanner (yellow wire)
 
 
 
